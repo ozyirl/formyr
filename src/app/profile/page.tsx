ea94686818bddb0b12b902a24835b6d0c4c74e6c
Galaxyraider20 <miharia.rachit2@gmail.com>
2025-09-27 23:05:36 -0700

    modification:changed forms list from cards to list

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,26 +2,23 @@
 
 import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 
-import { ModeToggle } from "@/components/mode-toggle";
-
 const Profile = () => {
-  return (
-    <>
-      <div className="flex items-center gap-3 justify-center h-screen">
-        <SignedIn>
-          <div className="flex items-center gap-3">
-            <span className="text-lg font-semibold text-foreground">
-              Profile
-            </span>
-            <ModeToggle />
-          </div>
-        </SignedIn>
-      </div>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-    </>
-  );
+	return (
+		<>
+			<div className="flex items-center gap-3 justify-center h-screen">
+				<SignedIn>
+					<div className="flex items-center gap-3">
+						<span className="text-lg font-semibold text-foreground">
+							Profile
+						</span>
+					</div>
+				</SignedIn>
+			</div>
+			<SignedOut>
+				<RedirectToSignIn />
+			</SignedOut>
+		</>
+	);
 };
 
 export default Profile;
